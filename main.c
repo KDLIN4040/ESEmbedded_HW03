900c800a8dec1183b5dbf77b8331067666dbae8c
KDLIN4040 <h72jas@gmail.com>
2019-03-14 17:03:04 +0800

    first

--- main.c
+++ main.c
@@ -1,5 +1,20 @@
+void manyint(int a, int b, int c, int d, int e )
+{
+			int nothing = a+b+c+d+e ;
+}
+
+void threeint(int a, int b, int c)
+{
+			int nothing = a+b+c;
+}
+
+int myreturn(int a)
+{
+			return a+a;
+}
 void reset_handler(void)
 {
+	int value = myreturn(1);
 	while (1)
 		;
 }
