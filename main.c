cca9d739ebb838a791965db9fca6f7088d509932
KDLIN4040 <h72jas@gmail.com>
2019-03-15 17:43:09 +0800

    manyreturn

--- main.c
+++ main.c
@@ -1,20 +1,27 @@
-void manyint(int a, int b, int c, int d, int e )
-{
-			int nothing = a+b+c+d+e ;
-}
-
-void threeint(int a, int b, int c)
-{
-			int nothing = a+b+c;
-}
-
-int myreturn(int a)
+int oneint(int a)
 {
 			return a+a;
 }
+
+int manyint(int a, int b, int c, int d, int e, int f)
+{
+	        return a+b+c+d+e;
+}
+
+int manyreturn(int a, int b, int *add, int *sub, int *mul, int *divi, int *square) 
+{
+			*add = a+b ;
+			*sub = a-b ;
+			*mul = a*b ;
+			*divi = a/b ;
+			*square =a^b;
+}
 void reset_handler(void)
 {
-	int value = myreturn(1);
+    int add, sub, mul, divi, square ;
+    oneint(1);
+    manyint(1, 2, 3, 4, 5, 6);
+	manyreturn(1, 2, &add, &sub, &mul, &divi, &square);
 	while (1)
 		;
 }
